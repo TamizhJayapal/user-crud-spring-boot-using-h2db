eb2240bc4ab0ddccf8495344082c32ae7839c830
TamizhJayapal <ajtamizh96@gmail.com>
2023-12-17 12:10:05 +0530

    Added Mysql connector and datasource for sql as well

--- src/main/java/com/example/springbootautharcrudapp/model/Authar.java
+++ src/main/java/com/example/springbootautharcrudapp/model/Authar.java
@@ -3,66 +3,23 @@ package com.example.springbootautharcrudapp.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
-@Table(name="Authar")
-
+@Table(name="authar")
 public class Authar {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue
 	private int id;
-	
+
 	private String name;
-	
+
 	private String email;
-	
+
 	private String country;
 
-	public Authar() {
-	}
-
-	public Authar(int id, String name, String email, String country) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.country = country;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	@Override
-	public String toString() {
-		return "Authar [id=" + id + ", name=" + name + ", email=" + email + ", country=" + country + "]";
-	}
-	
-	
 }
